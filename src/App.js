e1b947943b1f6d0123ef8db7fa9cfedb61f6a656
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-11 00:30:06 +0100

    An update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Grid2 } from "@mui/material";
 import youtube from "./api/youtube";
 import SearchBar from "./Components/SearchBar";
+import VideoDetail from "./Components/VideoDetail";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState({ id: {}, snippet: {} });
   return (
     <Grid2 justifyContent={"center"} container spacing={10}>
       <Grid2 size={{ xs: 11 }}>
         <Grid2 size={{ xs: 12 }}>
           <SearchBar onSubmit={handleSubmit} />
         </Grid2>
-        <Grid2 size={{ xs: 8 }}>{/* {VideoDetail} */}</Grid2>
+        <Grid2 size={{ xs: 8 }}>
+          <VideoDetail selectedVideo={selectedVideo} />
+        </Grid2>
         <Grid2 size={{ xs: 4 }}>{/* {VideoList} */}</Grid2>
       </Grid2>
     </Grid2>
   );
   async function handleSubmit(searchTerm) {
-    const response = await youtube.get("search", {
+    const {
+      data: { items: videos },
+    } = await youtube.get("search", {
       params: {
         part: "snippet",
         maxResults: 5,
@@ -25,7 +32,8 @@ function App() {
         q: searchTerm,
       },
     });
-    console.log("response", response.data.items);
+    setVideos(videos);
+    setSelectedVideo(videos[0]);
   }
 }
 
--- src/Components/VideoDetail.jsx
+++ src/Components/VideoDetail.jsx
@@ -1,6 +1,25 @@
-const VideoDetail = ()=>{
-    return (
-
-    )
-}
+import { Paper, Typography } from "@mui/material";
+const VideoDetail = ({
+  video: {
+    id: { videoID },
+    snippet: { title, channelTitle, description },
+  },
+}) => {
+  if (!videoID) {
+    return <div>Loading...</div>;
+  }
+  const videoSrc = `https://www.youtube.com/embed/${videoID}`;
+  return (
+    <React.Fragment>
+      <Paper elevation={6} style={{ height: "70%" }}>
+        <iframe
+          style={{ border: "none", height: "100%", width: "100%" }}
+          title="Video Player"
+          src={videoSrc}
+        />
+      </Paper>
+      <Paper elevation={6} style={{ padding: "15px" }}></Paper>
+    </React.Fragment>
+  );
+};
 export default VideoDetail;
